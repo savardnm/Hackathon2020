3d9218d3c7a600f028601793354dd037485b4d37
Nathan Savard <savardnm@gmail.com>
2020-01-17 22:13:50 -0500

    Bug Fixing

--- get_availabilities.js
+++ get_availabilities.js
@@ -29,9 +29,9 @@ function getRoomAvailHours() {
 	var today = new Date()
 	var hour = today.getHours();
 	if (hour < 10) {
-		hour = "0" + toString(hour);
+		hour = "0" + hour.toString();
 	} else {
-		hour = toString(hour);
+		hour = hour.toString();
 	}	
 	var startTime = toString(today.getFullYear()) + "-" + toString(today.getMonth()) + "-" + toString(today.getDate()) + "T" + hour + ":00:00";
 	var endTime = toString(today.getFullYear()) + "-" + toString(today.getMonth() + 1) + "-" + toString(today.getDate()) + "T" + hour + ":00:00";
