11226611c88c117638fadec4244cd7ed1850f5de
Avery Smith <ajsmith2@wpi.edu>
2020-01-17 19:15:35 -0500

    index made

---
+++ ids.js
@@ -0,0 +1,13 @@
+var live_base_url = "https://25live.collegenet.com/25live/data/wpi/run/rm_reservations.xml?space_id="
+
+var live_start_time_url = "&start_dt="
+
+var live_end_time_url = ""
+
+var rooms = {
+	5:"AK 116"
+	305:"AK 126A"
+	7:"AK 219"
+	8:"AK 232"
+	9:"AK 233"
+};
