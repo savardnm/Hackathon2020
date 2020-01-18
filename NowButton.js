4afbb156d145c05022e5b59d73956e7d2bbd6aee
maruthema <oceanicmadison@gmail.com>
2020-01-17 20:59:43 -0500

    i made things centered
    yep

--- NowButton.js
+++
@@ -1,9 +0,0 @@
-var button=document.createElement("button");
-button.innerHTML="Find Classroom";
-
-var body=document.getElementByTagName("body")[0];
-body.appendChild(button);
-
-button.addEventListener("click",function(){
-    alert("AAAAHHHHH")
-});
