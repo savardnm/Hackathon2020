94109360acd422192e794f4e44d21f2e46feef36
maruthema <oceanicmadison@gmail.com>
2020-01-17 20:08:41 -0500

    yep
    idk what i did

---
+++ NowButton.js
@@ -0,0 +1,9 @@
+var button=document.createElement("button");
+button.innerHTML="Find Classroom";
+
+var body=document.getElementByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener("click",function(){
+    alert("AAAAHHHHH")
+});
