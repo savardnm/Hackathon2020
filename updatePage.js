1babd9a8ac6d88ff16ea8f4e087a4bcd2c10bfc5
Nathan Savard <savardnm@gmail.com>
2020-01-18 14:55:27 -0500

    Added R titles to the rows

--- updatePage.js
+++ updatePage.js
@@ -4,46 +4,51 @@ var CURRENT_PAGE = 0;
 var roomsOpen = getAllRoomsOpenLength(new Date(), new Date(new Date().getTime() + 24 * 60 * 60 * 1000));
 
 function checkAllResponsesIn(responseCount) {
-	if (responseCount == Object.keys(rooms).length) {
-		var loadSymbol = document.getElementById("loadingSymbol");
-		var grey = document.getElementById("grey");
-		loadSymbol.parentNode.removeChild(loadSymbol);;
-		grey.parentNode.removeChild(grey);
-		roomsOpen = fillTable();
-		updateListing();
-	}
+    if (responseCount == Object.keys(rooms).length) {
+        var loadSymbol = document.getElementById("loadingSymbol");
+        var grey = document.getElementById("grey");
+        loadSymbol.parentNode.removeChild(loadSymbol);
+        ;
+        grey.style.opacity = "0";
+        roomsOpen = fillTable();
+        updateListing();
+    }
 }
 
 function fillTable() {
-	var table = []
-	
-	var count = 0;
-	for (var key in roomsOpen) {
-		if (roomsOpen[key] != 0) {
-			if (count % 5 == 0) {
-				var page = {};
-				page[key] = roomsOpen[key];
-				table.push(page);
-			} else {
-				table[Math.floor(count/TABLE_ROWS)][key] = roomsOpen[key];
-			}
-			count++;
-		}
-	}
-	return table;
+    var table = []
+
+    var count = 0;
+    for (var key in roomsOpen) {
+        if (roomsOpen[key] != 0) {
+            if (count % 5 == 0) {
+                var page = {};
+                page[key] = roomsOpen[key];
+                table.push(page);
+            } else {
+                table[Math.floor(count / TABLE_ROWS)][key] = roomsOpen[key];
+            }
+            count++;
+        }
+    }
+    return table;
 }
 
 function updateListing() {
-	var page = roomsOpen[CURRENT_PAGE];
-	var count = 1;
-	for (var key in page) {
-		document.getElementById("roomR" + count).innerHTML = rooms[key];
-		document.getElementById("hoursR" + count).innerHTML = page[key] + " hours";
-		document.getElementById("imgR" + count).src = roomPics[key];
-		document.getElementById("linkR" + count).href = "https://25live.collegenet.com/pro/wpi#!/home/location/" + key + "/details";
-		count++;
-	}
-	if (roomsOpen.length > 1) {
-		document.getElementById('rButton').disabled = false;
-	}
+    var page = roomsOpen[CURRENT_PAGE];
+    var count = 1;
+    for (var key in page) {
+        document.getElementById("roomR" + count).innerHTML = rooms[key];
+        document.getElementById("hoursR" + count).innerHTML = page[key] + " hours";
+        //document.getElementById("imgR" + count).src = roomPics[key];
+        count++;
+    }
+    if (roomsOpen.length > 1) {
+        document.getElementById('rButton').disabled = false;
+    }
+}
+
+function openImage(pos) {
+    document.getElementById("imgR" + pos).style.visibility = "visible";
+	document.getElementById("grey").style.opacity = "0.2";
 }
